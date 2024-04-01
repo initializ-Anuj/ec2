6912e60d9748c768cd6b911838a49036df5ac3e2
initializ-Anuj <141613996+initializ-Anuj@users.noreply.github.com>
2024-04-01 18:04:29 +0530

    Update main.tf
    this is my new commit

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
-provider "aws" {
+terraform {
+  required_version = ">= 0.12.24"
+
+  backend "s3" {
+    bucket = "anujs-8623tf"
+    key    = "anuj-8623tf.tfstate"
     region = "ap-south-1"
+  }
 }
 
-resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    count = "${var.number_of_instances}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.ami_key_pair_name}"
+provider "aws" {
+  region = "ap-south-1"
 }
-
+   
